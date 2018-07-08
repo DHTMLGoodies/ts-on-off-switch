d37c0b37b835e8e10cd62975f57811379468012d
Alf Magne Kalleland <alf@papirfly.no>
2018-07-08 17:18:57 +0200

    Add callback unit test and readme

---
+++ readme.md
@@ -0,0 +1,7 @@
+# JQuery On-Off Switch written in Typescript
+
+
+
+# Vulnerable dependencies
+
+These dependencies are caused by requirements in the Karma test runner framework. 
--- test/OnOffSwitch.test.ts
+++ test/OnOffSwitch.test.ts
@@ -20,9 +20,36 @@ describe("MinMaxSlider", () => {
         });
 
         // then
-        assert.ok(onOffSwitch);
-
+        assert.exists(onOffSwitch);
     });
 
+    it("should trigger callback", () => {
+        let triggeredName: string;
+        let triggeredValue: boolean;
+        let onOffSwitch = new OnOffSwitch({
+            el: $('<input type="checkbox" name="checkbox">'),
+            textOn: "on",
+            textOff: "off",
+            listener: (name: string, checked: boolean) => {
+                triggeredName = name;
+                triggeredValue = checked;
+
+            }
+        });
+
+        // when
+        onOffSwitch.check();
+
+        // then
+        assert.equal(triggeredName, "checkbox");
+        assert.isTrue(triggeredValue);
+
+        // when
+        onOffSwitch.uncheck();
+
+        // then
+        assert.isFalse(triggeredValue);
+
+    });
 
 });
